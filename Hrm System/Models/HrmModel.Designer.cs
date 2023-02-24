8b8e3258bae37d7a040191424ec55ed48b4194e9
Ashbel Handiringi <ashbel.handiringi@gmail.com>
2023-02-24 19:53:11 +0200

    .net4.8 upgrade

--- Hrm System/Models/HrmModel.Designer.cs
+++ Hrm System/Models/HrmModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\ashbel\Documents\Visual Studio 2012\Projects\Hrm System\Hrm System\Models\HrmModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ashbe\source\repos\HRM\Hrm System\Models\HrmModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
